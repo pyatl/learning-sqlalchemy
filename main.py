5e4bb86e79a83c1aff35cc22d333ef2295719c09
pryelluw <pryelluw@gmail.com>
2020-08-25 20:45:04 -0400

    select

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, select
 from models import User, Address
 
 engine = create_engine('sqlite:///:memory:', echo=True)
@@ -26,7 +26,7 @@ my_other_user = User(name='goishen', fullname='goishen the barbarian')
 insert_user = users.insert().values(name=my_user.name, fullname=my_user.fullname)
 insert_other_user = users.insert().values(name=my_other_user.name, fullname=my_other_user.fullname)
 
-# execute statements
+# execute insert statements statements
 
 conn = engine.connect()
 
@@ -35,3 +35,22 @@ result_two = conn.execute(insert_other_user)
 
 print(result_one.inserted_primary_key)
 print(result_two.inserted_primary_key)
+
+
+# execute select statements
+
+select_users = select([users])
+users_result = conn.execute(select_users)
+
+users_list = []
+for row in users_result:
+    users_list.append(
+        User(
+            id=row[0],
+            name=row[1],
+            fullname=row[2]
+        )
+    )
+
+for my_list_user in users_list:
+    print(my_list_user.id, my_list_user.name, my_list_user.fullname)
